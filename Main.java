56dbdcbd060ff3526254f0d330f5042a7f85d82e
Natalia Klimareva <60422598+MIA1kl@users.noreply.github.com>
2020-02-04 14:13:14 +0600

    Dont stop until clicking S

--- Main.java
+++ Main.java
@@ -2,14 +2,13 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-	for ( int i=1; i<=100; i++){
-	    System.out.print("Divisors of number " +i+ " is: ");
-	    for (int j=1;j<=i;j++)
-	        if (i%j==0) System.out.print(j+", ");
-	            System.out.println();
-
+    public static void main(String[] args)
+            throws java.io.IOException{
+                int i;
+                System.out.println("Click S");
+                for(i=0; (char)System.in.read()!='s';i++) {
+                    System.out.println(i);
         }
+
     }
 }
